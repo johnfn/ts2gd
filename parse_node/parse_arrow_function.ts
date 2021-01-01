876dd54ddb628f946be377b1afa5fa1f303d6ce5
johnfn <johnfn@gmail.com>
2021-01-01 13:07:09 -0500

    Add scope capturing to arrow functions

--- parse_node/parse_arrow_function.ts
+++ parse_node/parse_arrow_function.ts
@@ -1,13 +1,119 @@
-import ts, { SyntaxKind } from "typescript";
-import { combine, ParseState } from "../parse_node";
+import ts, { SyntaxKind } from "typescript"
+import { combine, ParseState } from "../parse_node"
 import { ParseNodeType } from "../parse_node"
 
-export const parseArrowFunction = (node: ts.ArrowFunction, props: ParseState): ParseNodeType => {
-  const name = props.genUniqueName();
+/**
+ * Get all identifiers in a scope that were declared in an enclosing scope.
+ *
+ * e.g.
+ *
+ * function foo() {
+ *   let a = 1;
+ *   return a + b + 1;
+ * }
+ *
+ * in foo(), `a` is not a free variable, but `b` is.
+ */
+const getFreeVariables = (
+  node: ts.Node | undefined | null,
+  typeChecker: ts.TypeChecker,
+  root: ts.ArrowFunction
+): (ts.Identifier | ts.PropertyAccessExpression)[] => {
+  if (!node) {
+    return []
+  }
 
-  let parsed: ParseNodeType;
+  if (
+    node.kind === SyntaxKind.Identifier ||
+    node.kind === SyntaxKind.PropertyAccessExpression
+  ) {
+    if (node.kind === SyntaxKind.PropertyAccessExpression) {
+      // In cases like "a.b.c", only return "a".
+      const pae = node as ts.PropertyAccessExpression
+      node = pae.expression
+    }
 
-  parsed = combine({
+    const symbol = typeChecker.getSymbolAtLocation(node)
+
+    if (symbol) {
+      const decl = symbol.declarations[0]
+
+      if (decl.getSourceFile() !== root.getSourceFile()) {
+        return []
+      }
+
+      let currentParent: ts.Node | undefined = decl
+      let isFreeVariable = true
+
+      while (currentParent) {
+        if (currentParent === root) {
+          isFreeVariable = false
+
+          break
+        }
+
+        currentParent = currentParent.parent
+      }
+
+      if (isFreeVariable) {
+        return [node as ts.Identifier | ts.PropertyAccessExpression]
+      } else {
+        return []
+      }
+    } else {
+      console.log(node.getText(), "no symbol")
+    }
+
+    return []
+  }
+
+  return node
+    .getChildren()
+    .flatMap((child) => getFreeVariables(child, typeChecker, root))
+}
+
+export const getCapturedScope = (
+  node: ts.ArrowFunction,
+  checker: ts.TypeChecker
+): {
+  capturedScopeObject: string
+  unwrapCapturedScope: string
+} => {
+  const freeVariables = getFreeVariables(node.body, checker, node)
+
+  const uniqueFreeVariables = freeVariables.filter(
+    (item, index) => freeVariables.indexOf(item) === index
+  )
+
+  const capturedScopeObject =
+    "{" +
+    uniqueFreeVariables
+      .map((freeVar) => `"${freeVar.getText()}": ${freeVar.getText()}`)
+      .join(", ") +
+    "}"
+
+  const unwrapCapturedScope = uniqueFreeVariables
+    .map((v) => `  var ${v.getText()} = captures.${v.getText()}\n`)
+    .join("")
+
+  return {
+    capturedScopeObject,
+    unwrapCapturedScope,
+  }
+}
+
+export const parseArrowFunction = (
+  node: ts.ArrowFunction,
+  props: ParseState
+): ParseNodeType => {
+  const name = props.genUniqueName()
+
+  const { unwrapCapturedScope } = getCapturedScope(
+    node,
+    props.program.getTypeChecker()
+  )
+
+  let parsed = combine({
     parent: node,
     nodes: [node.body, ...node.parameters],
     props,
@@ -15,17 +121,18 @@ export const parseArrowFunction = (node: ts.ArrowFunction, props: ParseState): P
     content: (body, ...args) => {
       if (node.body.kind === SyntaxKind.Block) {
         return `
-func ${name}(${args.join(', ')}):
+func ${name}(${[...args, "captures"].join(", ")}):
+${unwrapCapturedScope}
   ${body}
-        `;
+        `
       } else {
         // Single line arrow function, with implicit return.
 
         return `
-func ${name}(${args.join(', ')}):
+func ${name}(${[...args, "captures"].join(", ")}):
+${unwrapCapturedScope}
   return ${body}
-        `;
-
+        `
       }
     },
   })
@@ -34,7 +141,7 @@ func ${name}(${args.join(', ')}):
     content: `funcref(self, "${name}")`,
     hoistedArrowFunctions: [
       parsed.content,
-      ...(parsed.hoistedArrowFunctions ?? [])
+      ...(parsed.hoistedArrowFunctions ?? []),
     ],
   }
 }
--- parse_node/parse_call_expression.ts
+++ parse_node/parse_call_expression.ts
@@ -1,78 +1,105 @@
-import ts, { SyntaxKind, TypeFlags } from "typescript";
-import { combine, ParseState } from "../parse_node";
+import ts, { SyntaxKind, TypeFlags } from "typescript"
+import { combine, ParseState } from "../parse_node"
 import { ParseNodeType } from "../parse_node"
-import { Test } from "../test";
-import { isArrayType } from "../ts_utils";
+import { Test } from "../test"
+import { isArrayType } from "../ts_utils"
+import { getCapturedScope } from "./parse_arrow_function"
 
-type LibraryFunctionNames = 'map' | 'filter';
-const LibraryFunctions = {
-  'map': {
-    name: 'map',
-    definition: `
-func __map(list, fn):
+type LibraryFunctionName = "map" | "filter"
+const LibraryFunctions: {
+  [key in LibraryFunctionName]: {
+    name: LibraryFunctionName
+    definition: (name: string) => string
+  }
+} = {
+  map: {
+    name: "map",
+    definition: (name: string) => `
+func ${name}(list, fn, captures):
   var result = []
 
   for item in list:
-    result.append(fn.call_func(item))
+    result.append(fn.call_func(item, captures))
 
   return result
-    `
+    `,
   },
 
-  'filter': {
-    name: 'filter',
-    definition: `
-func __filter(list, fn):
+  filter: {
+    name: "filter",
+    definition: (name: string) => `
+func ${name}(list, fn, captures):
   var result = []
 
   for item in list:
-    if fn.call_func(item):
+    if fn.call_func(item, captures):
       result.append(item)
 
   return result
-    `
+    `,
   },
 }
 
-export const parseCallExpression = (node: ts.CallExpression, props: ParseState): ParseNodeType => {
-  let expression = node.expression;
-  let args = node.arguments;
+export const parseCallExpression = (
+  node: ts.CallExpression,
+  props: ParseState
+): ParseNodeType => {
+  let expression = node.expression
+  let args = node.arguments
 
   if (node.expression.kind === SyntaxKind.SuperKeyword) {
     return combine({
       parent: node,
       nodes: node.expression,
       props,
-      content: () => ""
-    });
+      content: () => "",
+    })
   }
 
   // This block compiles vec.add(vec2) into vec + vec2.
 
   // node = [[ a.b(c) ]]
   if (node.expression.kind === SyntaxKind.PropertyAccessExpression) {
-
     // prop = [[ a.b ]](c)
-    const prop = node.expression as ts.PropertyAccessExpression;
-    const functionName = prop.name.getText();
+    const prop = node.expression as ts.PropertyAccessExpression
+    const functionName = prop.name.getText()
 
-    const type = props.program.getTypeChecker().getTypeAtLocation(prop.expression);
-    const stringType = props.program.getTypeChecker().typeToString(type);
+    const type = props.program
+      .getTypeChecker()
+      .getTypeAtLocation(prop.expression)
+    const stringType = props.program.getTypeChecker().typeToString(type)
 
     if (isArrayType(type)) {
       if (functionName in LibraryFunctions) {
+        const libFunctionName = functionName as LibraryFunctionName
+
+        if (node.arguments[0].kind !== SyntaxKind.ArrowFunction) {
+          throw new Error("Can't do map w/o an arrow function!")
+        }
+
+        const arrowFunction = node.arguments[0] as ts.ArrowFunction
+
+        const { capturedScopeObject } = getCapturedScope(
+          arrowFunction,
+          props.program.getTypeChecker()
+        )
+
         const result = combine({
           parent: node,
           nodes: [prop.expression, ...args],
           props,
           content: (expr, ...args) => {
-            return `__${functionName}(${[expr, ...args].join(', ')})`;
-          }
-        });
+            return `__${libFunctionName}(${[expr, ...args].join(
+              ", "
+            )}, ${capturedScopeObject})`
+          },
+        })
 
-        result.hoistedLibraryFunctions = [(LibraryFunctions as any)[functionName].definition];
+        result.hoistedLibraryFunctions = [
+          LibraryFunctions[libFunctionName].definition("__" + libFunctionName),
+        ]
 
-        return result;
+        return result
       }
     }
 
@@ -82,12 +109,12 @@ export const parseCallExpression = (node: ts.CallExpression, props: ParseState):
       stringType === "Vector3" ||
       stringType === "Vector3i"
     ) {
-      let operator: undefined | string = undefined;
+      let operator: undefined | string = undefined
 
-      if (functionName === "add") operator = "+";
-      if (functionName === "sub") operator = "-";
-      if (functionName === "mul") operator = "*";
-      if (functionName === "div") operator = "/";
+      if (functionName === "add") operator = "+"
+      if (functionName === "sub") operator = "-"
+      if (functionName === "mul") operator = "*"
+      if (functionName === "div") operator = "/"
 
       if (operator) {
         return combine({
@@ -95,14 +122,18 @@ export const parseCallExpression = (node: ts.CallExpression, props: ParseState):
           nodes: [prop.expression, node.arguments[0]],
           props,
           content: (exp, arg) => `${exp} ${operator} ${arg}`,
-        });
-
+        })
       }
     }
   }
 
-  const decls = props.program.getTypeChecker().getTypeAtLocation(node.expression).symbol?.declarations;
-  const isArrowFunction = decls && (decls[0].kind === SyntaxKind.ArrowFunction && decls[0].getSourceFile() === node.getSourceFile());
+  const decls = props.program
+    .getTypeChecker()
+    .getTypeAtLocation(node.expression).symbol?.declarations
+  const isArrowFunction =
+    decls &&
+    decls[0].kind === SyntaxKind.ArrowFunction &&
+    decls[0].getSourceFile() === node.getSourceFile()
 
   return combine({
     parent: node,
@@ -110,22 +141,27 @@ export const parseCallExpression = (node: ts.CallExpression, props: ParseState):
     props,
     content: (expr, ...args) => {
       if (expr === "Yield") {
-        expr = "yield";
+        expr = "yield"
       }
 
       if (isArrowFunction) {
-        return `${expr}.call_func(${args.join(', ')})`;
+        const { capturedScopeObject } = getCapturedScope(
+          decls[0] as ts.ArrowFunction,
+          props.program.getTypeChecker()
+        )
+
+        return `${expr}.call_func(${[...args, capturedScopeObject].join(", ")})`
       } else {
-        return `${expr}(${args.join(', ')})`;
+        return `${expr}(${args.join(", ")})`
       }
-    }
-  });
+    },
+  })
 }
 
 export const testBasicCall: Test = {
   ts: `foo("bar")`,
   expected: `foo("bar")`,
-};
+}
 
 export const testAddVec: Test = {
   ts: `const v1: Vector2; const v2: Vector2; v1.add(v2)`,
@@ -134,7 +170,7 @@ var v1
 var v2
 v1 + v2
 `,
-};
+}
 
 export const testAddVec2: Test = {
   expectFail: true,
@@ -144,7 +180,7 @@ var foo
 var v2
 foo['v'] + v2
 `,
-};
+}
 
 export const testNormalVec: Test = {
   ts: `const v1: Vector2; v1.distance_to(v1)`,
@@ -152,7 +188,7 @@ export const testNormalVec: Test = {
 var v1
 v1.distance_to(v1)
 `,
-};
+}
 
 export const testArrowFunction: Test = {
   ts: `
@@ -160,13 +196,12 @@ const test = () => 5;
 test()  
   `,
   expected: `
-func func1():
+func func1(captures):
   return 5
 var test = funcref(self, "func1")
-test.call_func()
+test.call_func({})
 `,
-};
-
+}
 
 export const testMap: Test = {
   ts: `
@@ -174,11 +209,41 @@ let x = [1, 2, 3]
 x.map(y => y * 3)
   `,
   expected: `
-${LibraryFunctions.map.definition}
-func func1(y: float):
+${LibraryFunctions.map.definition("__map")}
+func func1(y: float, captures):
   return y * 3
 var x = [1, 2, 3]
-__map(x, funcref(self, "func1"))
+__map(x, funcref(self, "func1"), {})
 `,
-};
+}
 
+export const testMapCapture: Test = {
+  ts: `
+let x = [1, 2, 3]
+let z = 5
+let big = { a : 6 }
+x.map(y => {
+  return z + big.a + y * 3
+})
+  `,
+  expected: `
+${LibraryFunctions.map.definition("__map")}
+func func1(y: float, captures):
+  var z = captures.z
+  var big = captures.big
+  return z + big.a + y * 3
+var x = [1, 2, 3]
+var z: int = 5
+var big = { "a": 6 }
+__map(x, funcref(self, "func1"), {"z": z, "big": big})
+`,
+}
+
+// TODO: this also fails lol
+// for (let i = 0; i < 3; i++) {
+
+// }
+// return z + big.a + y * 3
+
+// TODO this also fails bc it's double quoted.
+// let big = { "a" : 6 }
